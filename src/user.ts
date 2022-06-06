18013fbd8fe58cfaaf62d8df4cf8b9724f9ce9c9
SJCMN <sam.clavette@gmail.com>
2022-06-06 13:55:04 -0500

    faker, user class

--- src/user.ts
+++ src/user.ts
@@ -1,3 +1,16 @@
+import { faker } from '@faker-js/faker'
 class user {
-    
+    name: string;
+    location: {
+        lat: number,
+        long: number,
+    }
+
+    constructor(){
+        this.name = faker.name.firstName();
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            long: parseFloat(faker.address.longitude())
+        }
+    }
 }
