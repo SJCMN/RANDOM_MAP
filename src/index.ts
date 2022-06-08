c3cf35bf461197bac16ff19e441f13bb37830850
SJCMN <sam.clavette@gmail.com>
2022-06-08 15:43:55 -0500

    remove key, add env

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,34 @@
+import dotenv from 'dotenv';
 import { User } from "./user";
-import { Company } from "./company"
+import { Company } from "./company";
 
-const user = new User
-const company = new Company
+const user = new User;
+const company = new Company;
+const API_KEY = process.env.API_KEY;
+const script1 = document.createElement('script');
 
+script1.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
+
+dotenv.config();
+document.body.append(script1);
+
+
+console.log(script1, script1.src);
+
+console.log(process.env.API_KEY);
+console.log(dotenv);
 console.log(user);
 console.log(company);
+
+setTimeout(() => {
+    const map: HTMLElement = document.querySelector('#map')
+new google.maps.Map(map, {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+});
+
+}, 3000);
+
